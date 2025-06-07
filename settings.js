566a9b90b2b0d2cb58b60fd1182351713138f142
toothles-ai <tharushanamadara0000@gmail.com>
2025-06-07 18:42:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=st5ijJqB#dfUiHk0dnywzn7Jm6K0ThseGASBFuYIIv75o_06aMb4",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=dkBSRDKS#jdRdT5UBQ0a5KzoP3gZuC3zD0dmkxHvnWV-86bVFlYg",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
